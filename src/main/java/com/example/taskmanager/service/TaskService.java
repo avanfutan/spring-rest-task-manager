118a34c6b7020bfe2d3ec3601d021a6abaca6087
Avan Futan <avanfutan@gmail.com>
2025-06-16 17:39:03 +0700

    feat: init structure and implement task crud

---
+++ src/main/java/com/example/taskmanager/service/TaskService.java
@@ -0,0 +1,34 @@
+package com.example.taskmanager.service;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return repository.save(task);
+    }
+
+    public Task updateTask(Task task, Long id) {
+        return repository.findById(id).map((updatedTask) -> {
+            updatedTask.setTitle(task.getTitle());
+            updatedTask.setCompleted(task.isCompleted());
+            return repository.save(updatedTask);
+        }).orElseGet(() -> repository.save(task));
+    }
+
+    public void deleteTask(Long id) {
+        repository.deleteById(id);
+    }
+}
